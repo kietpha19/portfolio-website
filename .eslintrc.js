06b2e83bac7a5e1a548e1bb79c9c06cd7d32f213
kiet pha <85252510+kietpha19@users.noreply.github.com>
2024-03-25 14:25:09 -0500

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    plugins: ['react'],
+    extends: ['prettier', 'standard', 'plugin:react/recommended'],
+    env: {
+      browser: true,
+      es2021: true
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    overrides: [
+      {
+        env: {
+          node: true
+        },
+        files: ['eslint.config.js'],
+        parserOptions: {
+          sourceType: 'script'
+        }
+      }
+    ],
+    rules: {
+      semi: ['error', 'always']
+    }
+  };

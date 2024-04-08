f45e1fdae4ec365f924554ddc53c2eab2b02eb6e
kiet pha <85252510+kietpha19@users.noreply.github.com>
2024-04-07 21:34:13 -0500

    add some skills

--- src/infor/devSkills.js
+++ src/infor/devSkills.js
@@ -8,5 +8,55 @@ export const webSkills = [
 		icon: 'devicon-css3-plain colored',
 		name: 'CSS3',
 		level: 'Intermediate'
+	},
+	{
+		icon: 'devicon-bootstrap-plain colored',
+		name: 'bootstrap',
+		level: 'Beginner'
+	},
+	{
+		icon: 'devicon-npm-original-wordmark colored',
+		name: 'npm',
+		level: 'Intermediate'
+	},
+	{
+		icon: 'devicon-react-plain colored',
+		name: 'react',
+		level: 'Proficient'
+	},
+	{
+		icon: 'devicon-angular-plain colored',
+		name: 'angular',
+		level: 'Beginner'
+	},
+	{
+		icon: 'devicon-spring-original colored',
+		name: 'spring',
+		level: 'Proficient'
+	},
+	{
+		icon: 'devicon-maven-plain colored',
+		name: 'maven',
+		level: 'Proficient'
+	},
+	{
+		icon: 'devicon-django-plain colored',
+		name: 'django',
+		level: 'Beginner'
+	},
+	{
+		icon: 'devicon-mysql-original colored',
+		name: 'MySql',
+		level: 'Proficient'
+	},
+	{
+		icon: 'devicon-mongodb-plain colored',
+		name: 'MongoDb',
+		level: 'Intermediate'
+	},
+	{
+		icon: 'devicon-amazonwebservices-plain-wordmark colored',
+		name: 'AWS',
+		level: 'Intermediate'
 	}
 ];
--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -1,25 +1,27 @@
 // path: '/about'
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import DevSkill from '../components/about/DevSkill';
 import { webSkills } from '../infor/devSkills';
 
 const AboutPage = () => {
 	return (
-		<div>
+		<div className='about-page'>
 			<div>
 				<p>Years of Experience</p>
 				<p>1+</p>
 			</div>
 
-			<Container>
-				{webSkills.map((skill) => (
-					<Col key={skill.name}>
-						<DevSkill icon={skill.icon} name={skill.name} level={skill.level} />
-					</Col>
-				))}
+			<Container className='skills-container'>
+				<Row>
+					{webSkills.map((skill) => (
+						<Col key={skill.name}>
+							<DevSkill icon={skill.icon} name={skill.name} level={skill.level} />
+						</Col>
+					))}
+				</Row>
 			</Container>
 
 		</div>

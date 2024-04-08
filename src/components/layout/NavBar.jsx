d8b8d428fd91d91a0123b2a8cf3d4d28ea586322
anhkiet190999 <anhkiet190999@gmail.com>
2024-04-08 13:28:38 -0500

    fix navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
 
 const NavBar = () => {
 	return (
-		<Navbar expand="lg" className="bg-body-tertiary">
-			<Container className='nav-bar'>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* Toggle button */}
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className='me-auto' >
-						<Nav.Link as={NavLink} to="/">Home</Nav.Link>
-						<Nav.Link as={NavLink} to="/about">About</Nav.Link>
-						<Nav.Link as={NavLink} to="/projects">Projects</Nav.Link>
-						<Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-				<ThemeToggle/>
-			</Container>
-		</Navbar>
+		<Container fluid className='nav-bar'>
+			<Row>
+				<Col>
+					<NavLink to={'/'}>Home</NavLink>
+				</Col>
+				<Col>
+					<NavLink to={'/about'}>About</NavLink>
+				</Col>
+				<Col>
+					<NavLink to={'/project'}>Project</NavLink>
+				</Col>
+				<Col>
+					<NavLink to={'/contact'}>Contact</NavLink>
+				</Col>
+				<Col>
+					<ThemeToggle/>
+				</Col>
+			</Row>
+
+		</Container>
 	);
 };
 
